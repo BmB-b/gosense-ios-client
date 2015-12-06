ee8a018ca222c9e59340f7d9cc2f5f8d37368eba
ZhiFeng Hu <hufeng1987@gmail.com>
2015-12-06 20:02:22 +0800

    Delete unused comment

--- gosense-ios-client/main.m
+++ gosense-ios-client/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  gosense-ios-client
-//
-//  Created by HuZhiFeng on 15/11/19.
-//  Copyright © 2015年 netroby. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
